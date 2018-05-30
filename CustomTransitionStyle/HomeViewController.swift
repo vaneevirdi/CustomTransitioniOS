f3283006c2df84bb4b3c0314c126fbf023939ae3
Vanee Gupta <vanee.gupta@optus.com.au>
2018-05-30 21:04:41 +0530

    Added HomeViewController in main storyboard

--- CustomTransitionStyle/HomeViewController.swift
+++ CustomTransitionStyle/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  CustomTransitionStyle
 //
 //  Created by Vanee Virdi on 30/05/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
