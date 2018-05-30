006bdcd7f96fa4c44c47dfa4fc36a991ee4b9bec
Vanee Gupta <vanee.gupta@optus.com.au>
2018-05-30 19:46:15 +0530

    initial commit

---
+++ CustomTransitionStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTransitionStyle
+//
+//  Created by Vanee Virdi on 30/05/18.
+//  Copyright © 2018 Vanee Virdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
